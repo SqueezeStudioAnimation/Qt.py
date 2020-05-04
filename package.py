f7b12f7687fb195a41e7b1dac2da7cb5c9b12535
Carl Berube <cberube@squeezestudio.com>
2020-05-04 10:35:53 -0400

    Package Released To Version 1.0.2

--- package.py
+++ package.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 name = 'python_qtpy'
 
-version = '1.0.1'
+version = '1.0.2'
 
 description = 'Minimal Python 2 & 3 shim around all Qt bindings - PySide, PySide2, PyQt4 and PyQt5. (SqueezeStudioAnimation fork)'
 
